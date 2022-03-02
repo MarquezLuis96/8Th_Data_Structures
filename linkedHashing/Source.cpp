d9cb6bacdc58bfd016a369f8e9225a8435f8a2b6
Luis Marquez <englamc@gmail.com>
2022-03-02 15:15:51 -0400

    Adding class Element - Proved and workd well

--- linkedHashing/Source.cpp
+++ linkedHashing/Source.cpp
@@ -1,10 +1,17 @@
 /*
-* Programa: Hashing enlazado - Determinar la lista enlazada mas larga para 1000 números enteros
-*			generados aleatoriamente.
+* Programa:
 * 
+*				Determinar la lista enlazada más larga (mayor número de nodos)
+*	cuando se utiliza una tabla hash enlazada para guardar 1.000
+*	números enteros generados aleatoriamente. Escribir un programa
+*	que realice la tarea propuesta, considerar que el factor de carga
+*	es 1.
+*
 * Integrantes:
 *				Luis Márquez		-	C.I: 25.687.494
 *				Eliana Rodríguez	-	C.I: 00.000.000
+*
+* Fecha: 03/03/2022
 */
 
 //Librerias
@@ -13,8 +20,55 @@
 //Declaración de nombres de espacio
 using namespace std;
 
+class Element {
+	private:
+		int number;
+		Element* nextElement;
+
+	public:
+		//Constructor básico (No recibe nada)
+		Element() {
+			number = NULL;
+			nextElement = nullptr;
+		}
+		 
+		//Constructor sobrecargado 1 (recibe un int que será el número del elemento)
+		Element(int n) {
+			number = n;
+			nextElement = nullptr;
+		}
+
+		//Establece el entero recibido como el número del elemento
+		void setNumber(int n) {
+			number = n;
+		}
+
+		void setNextElement(Element* next) {
+			nextElement = next;
+		}
+
+		int getNumber() {
+			return number;
+		}
+
+		Element* getNextElement() {
+			return nextElement;
+		}
+};
+
 //Función principal
 int main(int args, char* argsv[]) {
-	cout << "Hello World" << endl;
+	
+	Element a, b, c;
+
+	a.setNumber(1);
+	b.setNumber(2);
+	c.setNumber(3);
+
+	a.setNextElement(&b);
+	b.setNextElement(&c);
+
+	cout << "Claves enlazadas" << endl;
+
 	return 0;
 }
